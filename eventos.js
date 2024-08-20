dbbf699348c097b12234929fb03259c30d1e1071
atososa12 <antonellasosa125@gmail.com>
2024-08-19 21:33:46 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
 document.querySelector('div').addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
+document.querySelector('button').addEventListener('click', function(event) {
+    event.stopPropagation();  // Evita que el evento se propague al div
+    alert('¡Botón clickeado!');
+});
+
